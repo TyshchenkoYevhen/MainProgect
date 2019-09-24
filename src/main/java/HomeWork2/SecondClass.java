7efda791d44fad3d6cc53c2e4e08157f7752cde3
Yevhen Tyshchenko <tyschenko.evgeniy57@gmail.com>
2019-09-24 12:28:49 +0300

    first commit

--- src/main/java/HomeWork2/SecondClass.java
+++ src/main/java/HomeWork2/SecondClass.java
@@ -1,12 +1,115 @@
 package HomeWork2;
 
+//1. Print numbers from 10 to 20 using all known cycles.
+
 /*public class SecondClass {
+    public static void main (String [] args){
+        for (int i = 10; i <= 20; i++)
+            System.out.println(i);
+        }
+    }*/
+
+/*import java.util.ArrayList;
+
+class SecondClass {
+
     public static void main(String[] args) {
-        int i = 9;
-        while (i < 20) {
-            System.out.println(i + 1);
+
+        ArrayList list = new ArrayList<>();
+        list.add(10);
+        list.add(11);
+        list.add(12);
+        list.add(13);
+        list.add(14);
+        list.add(15);
+        list.add(16);
+        list.add(17);
+        list.add(18);
+        list.add(19);
+        list.add(20);
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+}*/
+
+/*import java.util.ArrayList;
+class SecondClass {
+
+    public static void main(String[] args) {
+
+        ArrayList list = new ArrayList<>();
+        list.add(10);
+        list.add(11);
+        list.add(12);
+        list.add(13);
+        list.add(14);
+        list.add(15);
+        list.add(16);
+        list.add(17);
+        list.add(18);
+        list.add(19);
+        list.add(20);
+
+        for (Object i:list) {
+            System.out.println(i);
+        }
+    }
+}*/
+
+/*public class SecondClass {
+    public static void main (String [] args){
+        int i = 10;
+        while ( i <= 20){
+            System.out.println(i);
             i++;
         }
     }
 }*/
 
+ /*class SecondClass {
+    public static void main (String [] args){
+        int i = 9;
+        do {
+            i++;
+            System.out.println(i);
+        }
+        while ( i < 20);
+        }
+    }*/
+
+/*class SecondClass {
+
+    public static void main(String[] args) {
+        int[] array = {10,11,12,13,14,15,16,17,18,19,20};
+
+        for (int i:array) {
+            System.out.println(i);
+        }
+    }
+}*/
+
+/*class SecondClass {
+    public static void main (String [] args){
+        for (int i = 10; i <= 20; i++){
+            if (i == 21)
+                break;
+            System.out.println(i);
+        }
+        }
+    }*/
+
+/*class SecondClass {
+    public static void main(String[] args) {
+        for (int i = 10; ; i++) {
+            if (i <= 20) {
+                System.out.println(i);
+                continue;
+            }
+            if (i == 21) {
+                break;
+            }
+        }
+    }
+}*/
