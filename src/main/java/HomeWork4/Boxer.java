010202a393ed5b09d8a06c05991f1ff8cc0344f4
Yevhen Tyshchenko <tyschenko.evgeniy57@gmail.com>
2019-09-28 22:58:45 +0300

    first commit

---
+++ src/main/java/HomeWork4/Boxer.java
@@ -0,0 +1,30 @@
+package HomeWork4;
+
+public class Boxer {
+    public int age;
+    public int weight;
+    public int strength;
+
+    public Boxer(int age, int weight, int strength) {
+        this.age = age;
+        this.weight = weight;
+        this.strength = strength;
+    }
+
+//    public boolean fight (a) {
+//        System.out.println(a);
+//
+//    }
+
+    public static void main(String[] args) {
+        Boxer Ali = new Boxer (20, 30,40);
+      //  Boxer Mike = new Boxer (30,40,50);
+    }
+      public static int sum (int a, int b, int c){
+        int sum = a + b + c;
+        return a;
+
+        }
+
+    }
+
---
+++ src/main/java/HomeWork4/TestSetGet.java
@@ -0,0 +1,16 @@
+package HomeWork4;
+
+public class TestSetGet {
+    public static void main (String [] args){
+
+        Dogs dog = new Dogs();
+
+        dog.setName("Bobik");
+        dog.setAge(-4);
+
+        System.out.println(dog.getName());
+        System.out.println(dog.getAge());
+
+
+    }
+}
