b6fc264edc6109f51e90d6352626d182b5e244f6
Yevhen Tyshchenko <tyschenko.evgeniy57@gmail.com>
2019-09-30 10:46:17 +0300

    first commit

--- src/main/java/HomeWork4/Boxer.java
+++ src/main/java/HomeWork4/Boxer.java
@@ -1,42 +1,42 @@
-/*package HomeWork4;
-
-
-class Boxer
-{
-    public int age;
-    public int weight;
-    public int strength;
-
-    public Boxer(int age, int weight, int strength)
-    {
-        this.age = age;
-        this.weight = weight;
-        this.strength = strength;
-    }
-
-    public boolean Fight(Boxer anotherBoxer) {
-        int myTotal = this.age + this.strength + this.weight;
-        int anotherTotal = anotherBoxer.age + anotherBoxer.strength + anotherBoxer.weight;
-
-        return myTotal > anotherTotal;
-    }
-
-     public static void main(String[] args)
-    {
-        Boxer ali = new Boxer(25, 100, 40);
-        Boxer mike = new Boxer(35, 80, 30);
-
-        boolean result = ali.Fight(mike);
-
-        boolean result2 = mike.Fight(ali);
-
-        if (result == true){
-            System.out.println("Победа Boxer");
-        }
-        if (result2 == true){
-            System.out.println("Победа anothrBoxer");
-        }
-
-    }
-}*/
-
+//package HomeWork4;
+//
+//
+//class Boxer
+//{
+//    public int age;
+//    public int weight;
+//    public int strength;
+//
+//    public Boxer(int age, int weight, int strength)
+//    {
+//        this.age = age;
+//        this.weight = weight;
+//        this.strength = strength;
+//    }
+//
+//    public boolean Fight(Boxer anotherBoxer) {
+//        int myTotal = this.age + this.strength + this.weight;
+//        int anotherTotal = anotherBoxer.age + anotherBoxer.strength + anotherBoxer.weight;
+//
+//        return myTotal > anotherTotal;
+//    }
+//
+//     public static void main(String[] args)
+//    {
+//        Boxer ali = new Boxer(25, 100, 40);
+//        Boxer mike = new Boxer(35, 80, 30);
+//
+//        boolean result = ali.Fight(mike);
+//
+//        boolean result2 = mike.Fight(ali);
+//
+//        if (result == true){
+//            System.out.println("Победа Boxer");
+//        }
+//        if (result2 == true){
+//            System.out.println("Победа anothrBoxer");
+//        }
+//
+//    }
+//}
+//
--- src/main/java/HomeWork4/Printer.java
+++ src/main/java/HomeWork4/Printer.java
@@ -1,4 +1,37 @@
 package HomeWork4;
 
-public class Printer {
-}
+ class Printer {
+
+     public int toner;
+     public int pages;
+
+     public Printer (int toner, int pages) {
+         this.toner = toner;
+         this.pages = pages;
+     }
+
+     public int fillToner() {
+         int j;
+         for (j = toner; j <= 100; j++) {
+             if (j == 100) {
+                 System.out.println("printer is filled on 100%");
+             } else {
+                 System.out.println("the printer is full on " + j + "%" );
+             }
+         }
+             return j;
+         }
+     public double PrintPage() {
+         int b = pages;
+         b++;{
+             System.out.println("Printed page # " + b);
+         }
+         return b;
+     }
+         public static void main (String[]args){
+             Printer ton = new Printer(98, 100);
+             int result = ton.fillToner();
+             Printer prin = new Printer(100, 85);
+             double result2 = prin.PrintPage();
+         }
+     }
