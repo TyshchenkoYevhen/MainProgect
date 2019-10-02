ff8dcc17ce2e2fdf3137996d148347ce2a0eb2ba
Yevhen Tyshchenko <tyschenko.evgeniy57@gmail.com>
2019-10-02 17:32:06 +0300

    first commit

--- src/main/java/HomeWork5/Task8.java
+++ src/main/java/HomeWork5/Task8.java
@@ -1,8 +1,11 @@
 package HomeWork5;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Task8 {
+/*public class Task8 {
 
     public static void main(String[] args) {
 
@@ -22,4 +25,24 @@ public class Task8 {
             System.out.println(" 'watermelon' + 'berry' " + myHashMap.containsKey("vegetable"));
         }
 
-    }
+    }*/
+
+public class Task8 {
+
+    public static void main(String[] args) {
+    Map<Integer, String> map = new HashMap<>();
+        map.put(10, "apple");
+        map.put(20, "orange");
+        map.put(30, "banana");
+
+        System.out.println("\n1. Export Map Key to List...");
+
+        List<Integer> result = new ArrayList(map.keySet());
+
+        result.forEach(System.out::println);
+
+        System.out.println("\n2. Export Map Value to List...");
+
+        List<String> result2 = new ArrayList(map.values());
+
+        result2.forEach(System.out::println);
