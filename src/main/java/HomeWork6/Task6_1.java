ac7e10c15281b78600338eada98dfb15c188c0e7
Yevhen Tyshchenko <tyschenko.evgeniy57@gmail.com>
2019-10-05 20:43:19 +0300

    first commit

--- src/main/java/HomeWork6/Task6_1.java
+++ src/main/java/HomeWork6/Task6_1.java
@@ -1,18 +1,24 @@
-//package HomeWork6;
-//
-//public class Task6_1 {
-//    public static void main (String [] args){
-//        int a = 0;
-//        if (a < 0) {
-//            System.out.println("Number < 0");
-//        }
-//        else if (a > 0){
-//            System.out.println("Number > 0");
-//        }
-//        try (
-//             if (a ==0);
-//
-//
-//        }
-//    }
-//}
+package HomeWork6;
+
+public class Task6_1 {
+
+    public static void main(String[] args) {
+        System.out.println(getNum(0));
+    }
+
+    public static int getNum(int a) throws IllegalArgumentException {
+        if (a == 0){
+            throw new IllegalArgumentException("Value can't be" + a);
+        }
+        {
+        if ((a > 0) || (a < 0)) {
+            System.out.println("Num > 0 or Num < 0");
+            return a;
+        }
+        else
+            System.out.println(a);
+            return a;
+        }
+    }
+}
+
---
+++ src/main/java/HomeWork6/Task6_4.java
@@ -0,0 +1,14 @@
+/*package HomeWork6;
+
+public class Task6_4 {
+    public static void main(String[] args) {
+        try {
+            int a = 0;
+            int b = 4 / a;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+        }
+    }
+}*/
