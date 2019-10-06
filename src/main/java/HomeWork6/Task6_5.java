6682553a7388ca9381c8b9c223e578094950c815
Yevhen Tyshchenko <tyschenko.evgeniy57@gmail.com>
2019-10-07 00:35:11 +0300

    first commit

--- src/main/java/HomeWork6/Task6_5.java
+++ src/main/java/HomeWork6/Task6_5.java
@@ -3,15 +3,17 @@ package HomeWork6;
 //5. Throw a NullPointerException from the method iCanThrowException();
 //Handle it in the main method.
 
-//public class Task6_5 {
-//
-//        public static void main(String[] args) {
-//            iCanThrowException();
-//        }
-//
-//        public static void iCanThrowException() {
-//
-//        }
-//
-//    }
+public class Task6_5 {
+
+        public static void main(String[] args) throws NullPointerException {
+            iCanThrowException();
+
+        }
+
+        public static void iCanThrowException() {
+            System.out.println(x);
+
+        }
+
+    }
 
